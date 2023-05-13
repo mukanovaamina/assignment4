ab05e0e160a45321a9e03eda0f4dfa63463b2e91
Admin <221250@astanait.edu.kz>
2023-05-13 19:40:51 +0600

    created new class MyTestingClass

---
+++ MyTestingClass.java
@@ -0,0 +1,34 @@
+public class MyTestingClass {
+    private int id;
+    private String name;
+
+    public MyTestingClass(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // Custom hashCode method
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
