1ed737668ab89d82122563fd3b8ffec3668ac51c
Admin <221250@astanait.edu.kz>
2023-05-14 18:29:24 +0600

    MyTestingClass

--- MyTestingClass.java
+++ MyTestingClass.java
@@ -7,5 +7,28 @@ public class MyTestingClass {
         this.name = name;
     }
 
+    public int hashCode() { //generate a hash code for an instance
+        int result = 17;
+        result = 31 * result + id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
 
+
+    public int getId() { //returns the value of the id instance variable
+        return id;
+    }
+
+    public void setId(int id) { //sets the value of the id instance variable
+        this.id = id;
+    }
+
+    public String getName() { //returns the value of the name instance variable
+        return name;
+    }
+
+    public void setName(String name) { // sets the value of the name instance variable
+        this.name = name;
+    }
 }
+
