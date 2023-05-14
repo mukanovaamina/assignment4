87381acc726b182dd339e40eed25b106db33d537
Admin <221250@astanait.edu.kz>
2023-05-14 18:22:16 +0600

    MyTestingClass with initializing

--- MyTestingClass.java
+++ MyTestingClass.java
@@ -7,28 +7,5 @@ public class MyTestingClass {
         this.name = name;
     }
 
-    // Custom hashCode method
-    public int hashCode() {
-        int result = 17;
-        result = 31 * result + id;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
-    }
 
-
-    public int getId() { //returns the value of the id instance variable
-        return id;
-    }
-
-    public void setId(int id) { //sets the value of the id instance variable
-        this.id = id;
-    }
-
-    public String getName() { //returns the value of the name instance variable
-        return name;
-    }
-
-    public void setName(String name) { // sets the value of the name instance variable
-        this.name = name;
-    }
 }
